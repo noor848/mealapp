1573a17595804147f7b5e1c223a339331deaaf85
noor848 <54281955+noor848@users.noreply.github.com>
2022-01-17 12:43:56 +0200

    adding files

---
+++ lib/modules/filter.dart
@@ -0,0 +1,105 @@
+
+
+import 'package:bma/modules/staticdata.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Filters extends StatefulWidget {
+
+  @override
+  _FiltersState createState() => _FiltersState();
+}
+
+class _FiltersState extends State<Filters> {
+
+  List<String>fi=["GlutenFree","Vegan","Vegetarian","LactoseFree"
+  ];
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+    children: [
+      ListTile(
+
+      trailing: Switch(
+    onChanged: (bool value) {
+
+    setState(() {
+      S_data.switch1=value;
+
+    });
+
+    }, value:S_data. switch1,
+    activeColor: Colors.orangeAccent,
+
+
+    ),
+    title:Text("GlutenFree")
+    ),
+      ListTile(
+
+          trailing: Switch(
+            onChanged: (bool value) {
+
+              setState(() {
+                S_data. switch2=value;
+
+              });
+
+            }, value:S_data.switch2,
+            activeColor: Colors.orangeAccent,
+
+
+          ),
+          title:Text("Vegan")
+      ),
+      ListTile(
+
+          trailing: Switch(
+            onChanged: (bool value) {
+
+              setState(() {
+                S_data. switch3=value;
+
+              });
+
+            }, value:S_data. switch3,
+            activeColor: Colors.orangeAccent,
+
+
+          ),
+          title:Text("LactoseFree")
+      ),
+      ListTile(
+          trailing: Switch(
+            onChanged: (bool value) {
+
+              setState(() {
+                S_data.   switch4=value;
+
+              });
+
+            }, value: S_data.switch4,
+            activeColor: Colors.orangeAccent,
+
+
+          ),
+          title:Text("Vegetarian")
+
+      )
+      ],
+    ));
+
+
+
+
+  }
+}
